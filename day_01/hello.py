242900925f96d424448fd2b803590683fd1843bc
aashu <ashutoshmalviya1000@gmail.com>
2021-01-04 12:04:25 +0530

    day_01: basic print,type and mathematical operation

---
+++ day_01/hello.py
@@ -0,0 +1,32 @@
+'''
+1. do simple mathematical operation
+2. write name,country,'i am enjoying 30 days of python'
+3. check the data type of some literals
+'''
+
+# mathematical operations
+print(f'23 + 2 = {23+2}')
+print(f'23 - 2 = {23-2}')
+print(f'23 * 2 = {23*2}')
+print(f'23 % 2 = {23%2}')
+print(f'23 / 2 = {23/2}')
+print(f'23 ** 2 = {23**2}')
+print(f'23 // 2 = {23//2}')
+
+# 2nd task
+name = 'Ashutosh Malviya'
+country_name = 'India'
+isEnjoying = True
+
+print(name)
+print(country_name)
+if isEnjoying:
+    print('I am enjoying 30 days of python')
+
+# 3rd task
+print(type(10))
+print(type(4.2))
+print(type(4-4j))
+print(type(['a',2,3.12,2j]))
+print(type(name))
+print(type(isEnjoying))
